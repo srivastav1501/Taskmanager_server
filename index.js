89263c5bd9eb17d47de3d7bf46d0fd3cb84c381e
Mukul Srivastav <mukulsrivastav551@gmail.com>
2023-06-14 15:48:12 +0530

    fourth commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import Path from 'path'
+import path from 'path'
 
 import Connection from './database/db.js';
 import Routes from './routes/route.js';
@@ -14,10 +14,10 @@ app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/', Routes);
-app.use(express.static(Path.join(__dirname, './client/build')))
+app.use(express.static(path.join(__dirname, './client/build')))
 
 app.get("*", function(req,res){
-    res.sendFile(Path.join(__dirname, './client/build/index.html'))
+    res.sendFile(path.join(__dirname, './client/build/index.html'))
 })
 
 const PORT = process.env.PORT || 8000;
