e5bfbe6f87c987dd8bf48291d2727226b49aab74
Mukul Srivastav <mukulsrivastav551@gmail.com>
2023-06-14 16:00:56 +0530

    seventh commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import { dirname } from 'path';
 import path from 'path'
 
 import Connection from './database/db.js';
