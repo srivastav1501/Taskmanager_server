08a9aa29633bee89523a9a8ac743bd5c8682b8e3
Mukul Srivastav <mukulsrivastav551@gmail.com>
2023-06-14 17:24:44 +0530

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import { dirname } from 'path';
-import path from 'path'
 
 import Connection from './database/db.js';
 import Routes from './routes/route.js';
@@ -15,11 +13,7 @@ app.use(cors());
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/', Routes);
-app.use(express.static(path.join(dirname, './client/build')))
 
-app.get("*", function(req,res){
-    res.sendFile(path.join(dirname, './client/build/index.html'))
-})
 
 const PORT = process.env.PORT || 8000;
 
